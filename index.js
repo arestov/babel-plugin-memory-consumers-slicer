76ea14ac4a330fcdef57b7c827d8afa6f4ad0d29
Gleb Arestov <gr1b0k@gmail.com>
2020-06-14 19:12:16 +0300

    refactor: move getFunctionName

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var {default: generate} = require("babel-generator");
 var isAmdBody = require('./src/isAmdBody')
+var getFunctionName = require('./src/getFunctionName')
 
 function docs(path) {
   // console.log(path.constructor.prototype)
@@ -9,48 +10,6 @@ function docBabel(bab) {
   // console.log('babel', Object.keys(bab))
 
 }
-
-function parentName(path) {
-  switch (path.node.type) {
-    case "VariableDeclarator":
-      return path.node.id.name
-    case "ObjectProperty":
-      return path.node.key.name
-    default:
-      return null
-  }
-}
-
-function getFunctionName(path) {
-  var func = path.findParent(path => {
-    switch (path.node.type) {
-      case "FunctionDeclaration":
-      case "FunctionExpression":
-      case "ArrowFunctionExpression":
-      case "ClassMethod":
-        return true
-      default: {
-        return false
-      }
-    }
-  })
-  if (!func) {
-    return
-  }
-  switch (func.node.type) {
-    case "FunctionDeclaration":
-      return func.node.id.name
-    case "ClassMethod":
-      return func.node.key.name
-    case "FunctionExpression":
-      return func.node.id ? func.node.id.name : parentName(func.parentPath)
-    case "ArrowFunctionExpression":
-      return parentName(func.parentPath)
-    default:
-      return null
-  }
-}
-
 
 module.exports = function logger(babel) {
   docBabel(babel)
